24f84288d2cd75b346a6fd00e24c5592c04426bf
IMaestro <iakimaestro@gmail.com>
2016-04-01 15:08:24 +0200

    two empty projects

---
+++ src/Q4S-client-server/ServerQ4S/ServerQ4S.cpp
@@ -0,0 +1,12 @@
+// Q4S-client-server.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include "windows.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Sleep(10000);
+	return 0;
+}
+
