639c812de438bded58096c79636ebc3b12bf1195
IMaestro <iakimaestro@gmail.com>
2016-05-27 10:04:55 +0200

    socket included

--- src/Q4S-client-server/ClientQ4S/ClientQ4S.cpp
+++ src/Q4S-client-server/ClientQ4S/ClientQ4S.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
-#include "windows.h"
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <Windows.h>
+
+#pragma comment(lib, "Ws2_32.lib")
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	
+//    Initialize Winsock.
+//    Create a socket.
+//    Connect to the server.
+//    Send and receive data.
+//    Disconnect.
+
 	Sleep(10000);
 	return 0;
 }
--- src/Q4S-client-server/ServerQ4S/ServerQ4S.cpp
+++ src/Q4S-client-server/ServerQ4S/ServerQ4S.cpp
@@ -6,6 +6,14 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+    //Initialize Winsock.
+    //Create a socket.
+    //Bind the socket.
+    //Listen on the socket for a client.
+    //Accept a connection from a client.
+    //Receive and send data.
+    //Disconnect.
+
 	Sleep(10000);
 	return 0;
 }
