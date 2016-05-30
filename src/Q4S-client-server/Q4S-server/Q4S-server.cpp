b90ec2e5dc7abe3877020e09460dcb53fbcd7057
IMaestro <iakimaestro@gmail.com>
2016-05-30 08:18:36 +0200

    VS 2010 Express project

---
+++ src/Q4S-client-server/Q4S-server/Q4S-server.cpp
@@ -0,0 +1,20 @@
+// Q4S-server.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "windows.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    //Initialize Winsock.
+    //Create a socket.
+    //Bind the socket.
+    //Listen on the socket for a client.
+    //Accept a connection from a client.
+    //Receive and send data.
+    //Disconnect.
+
+	Sleep(10000);
+	return 0;
+}
+
