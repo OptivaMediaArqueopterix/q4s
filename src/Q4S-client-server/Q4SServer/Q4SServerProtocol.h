87b65395452237f256f55d4997d49a0124f2f80e
IMaestro <iakimaestro@gmail.com>
2016-06-21 10:45:03 +0200

    server replying client pings

--- src/Q4S-client-server/Q4SServer/Q4SServerProtocol.h
+++ src/Q4S-client-server/Q4SServer/Q4SServerProtocol.h
@@ -46,6 +46,8 @@ private:
     static DWORD WINAPI         manageUdpReceivedDataFn( LPVOID lpData );
 
     Q4SMessageManager           mReceivedMessages;
+
+    bool                        isPingMessage(std::string message, int *pingNumber, unsigned long *timeStamp);
 };
 
 #endif  // _Q4SSERVERPROTOCOL_H_
