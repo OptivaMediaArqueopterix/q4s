60a458c36fc6348c7dfde7e4030d9a33d1869939
IMaestro <iakimaestro@gmail.com>
2016-10-20 14:49:30 +0200

    reading a configuration file but not global

--- src/Q4S-client-server/Q4SClient/Q4SClient.cpp
+++ src/Q4S-client-server/Q4SClient/Q4SClient.cpp
@@ -15,11 +15,31 @@
 
 #include <Windows.h>
 
+#include "ConfigFile.cpp"
+
 #pragma comment(lib, "Ws2_32.lib")
 
 int _tmain(int argc, _TCHAR* argv[])
 {
     bool ok = true;
+
+    {
+        ConfigFile cfg("Config.txt");
+
+        //bool exists = cfg.keyExists("SERVER_IP");
+        //std::cout << "keyName exits: " << std::boolalpha << exists << "\n";
+
+        std::string serverIP = cfg.getValueOfKey<std::string>("SERVER_IP", "127.0.0.1");
+        std::cout << "SERVER_IP: " << serverIP << "\n";
+        double defaultTCPPort = cfg.getValueOfKey<double>("DEFAULT_TCP_PORT", 27015);
+        std::cout << "DEFAULT_TCP_PORT: " << defaultTCPPort << "\n\n";
+        double defaultTUPPort = cfg.getValueOfKey<double>("DEFAULT_UDP_PORT", 27016);
+        std::cout << "DEFAULT_UDP_PORT: " << defaultTUPPort << "\n\n";
+
+        //std::cin.get();
+        //return 0;
+
+    }
 
     Q4SClientStateManager q4SClientStateManager;
 
