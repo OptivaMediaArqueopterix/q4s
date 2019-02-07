e545f33d173f34e5ee012f10e09d779f2054c73c
maroto <victor.maroto@optivamedia.com>
2019-02-07 17:00:06 +0100

    change bin folder

--- src/Q4S-client-server/bin/REQ1_startLogin.bat
+++
@@ -1,2 +0,0 @@
-call winhttpjs.bat "http://212.170.54.119:8280/policyserver-webcontroller/login"  -method POST  -saveTo token.txt -body-file auth.json
-echo DONE
--- src/Q4S-client-server/bin/REQ2_startSession.bat
+++
@@ -1,2 +0,0 @@
-call winhttpjs.bat "http://212.170.54.119:8280/policyserver-webcontroller/sessions/add"  -method POST   -header token.txt -saveTo response.file -body-file sdp.json
-echo DONE
